b99f85b12560760c6fc26cb8d8931a02ff1301e2
ignaciobriceno <ignacioob.10@gmail.com>
2020-02-17 21:46:17 -0300

    project

---
+++ db/migrate/20200217215828_add_admin_to_user.rb
@@ -0,0 +1,7 @@
+class AddAdminToUser < ActiveRecord::Migration[5.1]
+  def change
+  	add_column :users, :admin, :boolean, default: false
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+  end
+end
